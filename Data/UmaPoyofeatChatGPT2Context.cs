b84d408e33ce8876a40e15cab3864de6ed0b1e2c
jaso0125 <jaso0125@gmail.com>
2025-01-19 12:58:38 +0900

    データベースコンテキストの作成

---
+++ Data/UmaPoyofeatChatGPT2Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UmaPoyofeatChatGPT2.Models;
+
+namespace UmaPoyofeatChatGPT2.Data
+{
+    public class UmaPoyofeatChatGPT2Context : DbContext
+    {
+        public UmaPoyofeatChatGPT2Context(DbContextOptions<UmaPoyofeatChatGPT2Context> options) : base(options)
+        {
+        }
+
+        public DbSet<HorseRace> HorseRaces { get; set; }
+        public DbSet<PastRace> PastRaces { get; set; }
+        public DbSet<RaceInfo> RaceInfos { get; set; }
+        public DbSet<Result> Results { get; set; }
+    }
+}
